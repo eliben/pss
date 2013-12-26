aee4f4af780b7816256c9f478867c11f6693875d
Eli Bendersky <eliben@gmail.com>
2013-12-26 07:19:03 -0800

    cosmetic comment changes

--- tools/pss-bash-completion.bash
+++ tools/pss-bash-completion.bash
@@ -11,10 +11,11 @@ _pss_complete()
 {
     local cur_word prev_word type_list
 
-    # COMP_WORDS is an array of words in the current command line. COMP_CWORD
-    # is the index of the current word (the one the cursor is in).
-    # So COMP_WORDS[COMP_CWORD] is the current word; we also record the previous
-    # word here, although this specific script doesn't use it yet.
+    # COMP_WORDS is an array of words in the current command line.
+    # COMP_CWORD is the index of the current word (the one the cursor is
+    # in). So COMP_WORDS[COMP_CWORD] is the current word; we also record
+    # the previous word here, although this specific script doesn't
+    # use it yet.
     cur_word="${COMP_WORDS[COMP_CWORD]}"
     prev_word="${COMP_WORDS[COMP_CWORD-1]}"
 
@@ -24,8 +25,8 @@ _pss_complete()
     # Only perform completion if the current word starts with a dash ('-'),
     # meaning that the user is trying to complete an option.
     if [[ ${cur_word} == -* ]] ; then
-        # COMPREPLY is the array of possible completions, generated with the
-        # compgen builtin.
+        # COMPREPLY is the array of possible completions, generated with
+        # the compgen builtin.
         COMPREPLY=( $(compgen -W "${type_list}" -- ${cur_word}) )
     else
         COMPREPLY=()
