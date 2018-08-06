3a8a53bace9a20bd1cc856b220c3457e0fd6d92c
Eli Bendersky <eliben@gmail.com>
2018-08-06 06:20:48 -0700

    Update versions for 1.42 release

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     description='Tool for grepping through source code',
     long_description=description,
     license='Public domain',
-    version='1.41',
+    version='1.42',
     author='Eli Bendersky',
     maintainer='Eli Bendersky',
     author_email='eliben@gmail.com',
@@ -61,4 +61,3 @@ setup(
 
     **extra_args
 )
-
