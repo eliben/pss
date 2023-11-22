f6b8c0097a887cc00ab04aa2036037ddf6cd79db
Eli Bendersky <eliben@gmail.com>
2023-11-22 08:24:20 -0800

    Version 1.45

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     description='Tool for grepping through source code',
     long_description=description,
     license='Public domain',
-    version='1.44',
+    version='1.45',
     author='Eli Bendersky',
     maintainer='Eli Bendersky',
     author_email='eliben@gmail.com',
